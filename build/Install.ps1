6ea4b22ebe176a0f9afaff747d824cc682f8f960
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-08-26 09:56:47 +0100

    Fix so that Install.ps1 doesn't do anything for website projects

--- build/Install.ps1
+++ build/Install.ps1
@@ -3,16 +3,21 @@ param($installPath, $toolsPath, $package, $project)
 $appPluginsFolder = $project.ProjectItems | Where-Object { $_.Name -eq "App_Plugins" }
 $nestedContentFolder = $appPluginsFolder.ProjectItems | Where-Object { $_.Name -eq "NestedContent" }
 
-$newPackageFiles = "$installPath\Content\App_Plugins\NestedContent"
+if (!$nestedContentFolder)
+{
+	$newPackageFiles = "$installPath\Content\App_Plugins\NestedContent"
 
-$projFile = Get-Item ($project.FullName)
-$projDirectory = $projFile.DirectoryName
-$projectPath = Join-Path $projDirectory -ChildPath "App_Plugins"
-$projectPathExists = Test-Path $projectPath
+	$projFile = Get-Item ($project.FullName)
+	$projDirectory = $projFile.DirectoryName
+	$projectPath = Join-Path $projDirectory -ChildPath "App_Plugins"
+	$projectPathExists = Test-Path $projectPath
 
-if ($projectPathExists -and !$nestedContentFolder) {	
-	Write-Host "Updating Nested Content App_Plugin files using PS as they have been excluded from the project"
-	Copy-Item $newPackageFiles $projectPath -Recurse -Force
+	if ($projectPathExists) {	
+		Write-Host "Updating Nested Content App_Plugin files using PS as they have been excluded from the project"
+		Copy-Item $newPackageFiles $projectPath -Recurse -Force
+	}
 }
+
+
 
 
