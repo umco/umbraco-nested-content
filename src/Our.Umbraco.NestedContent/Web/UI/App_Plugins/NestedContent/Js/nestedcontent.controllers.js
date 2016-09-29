91d0c80df8313133d3ff938088db1e4ac1171bc3
Tom Pipe <tompipe@users.noreply.github.com>
2016-09-29 18:38:06 +0100

    Add feature to use {{index}} in a name template
    Added the ability to display the index position in a name template 
    
    e.g "Column {{index}}" resulting in "Column 1, Column 2 etc"

--- src/Our.Umbraco.NestedContent/Web/UI/App_Plugins/NestedContent/Js/nestedcontent.controllers.js
+++ src/Our.Umbraco.NestedContent/Web/UI/App_Plugins/NestedContent/Js/nestedcontent.controllers.js
@@ -200,7 +200,15 @@ angular.module("umbraco").controller("Our.Umbraco.NestedContent.Controllers.Nest
                 var contentType = $scope.getContentTypeConfig($scope.model.value[idx].ncContentTypeAlias);
 
                 if (contentType != null && contentType.nameExp) {
-                    var newName = contentType.nameExp($scope.model.value[idx]); // Run it against the stored dictionary value, NOT the node object
+                    var item = $scope.model.value[idx]; // Run it against the stored dictionary value, NOT the node object
+
+                    if (contentType.nameTemplate.indexOf("{{index}}") !== -1) {
+                        var cloneItem = JSON.parse(JSON.stringify(item));
+                        cloneItem.index = (idx + 1); // inject the index position to the object
+                        item = cloneItem;
+                    }
+
+                    var newName = contentType.nameExp(item);
                     if (newName && (newName = $.trim(newName))) {
                         name = newName;
                     }
